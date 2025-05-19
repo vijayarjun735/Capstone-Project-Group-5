e9df9109f549e7896f5bc74edc7361ac809e31b4
Arjun V <vijayarjun735@gmail.com>
2025-05-18 20:15:50 -0400

    App Base

---
+++ ChillCheck/ChillCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChillCheck
+//
+//  Created by Arjun V on 2025-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChillCheck/ChillCheckTests/ChillCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChillCheckTests.swift
+//  ChillCheckTests
+//
+//  Created by Arjun V on 2025-05-18.
+//
+
+import Testing
+@testable import ChillCheck
+
+struct ChillCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
