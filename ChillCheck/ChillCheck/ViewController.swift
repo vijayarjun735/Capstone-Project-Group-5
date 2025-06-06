2328fe751884c79031ced5fca5f61b4d3eb4230e
Vimal <Jangosliker@gmail.com>
2025-06-06 13:59:41 -0400

    update items

--- ChillCheck/ChillCheck/ViewController.swift
+++ ChillCheck/ChillCheck/ViewController.swift
@@ -20,68 +20,103 @@ class FridgeViewController: UIViewController {
         loadFridgeItems()
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadFridgeItems()
+    }
+    
+    private func setupUI() {
+        title = "My Fridge"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            loadFridgeItems()
-        }
-        
-        private func setupUI() {
-            title = "My Fridge"
-            navigationController?.navigationBar.prefersLargeTitles = true
-            
-            tableView.delegate = self
-            tableView.dataSource = self
-            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FridgeItemCell")
-        }
-        
-        private func loadFridgeItems() {
-            fridgeItems = FridgeDataManager.shared.loadFridgeItems()
-            tableView.reloadData()
-        }
-        
-        private func saveFridgeItems() {
-            FridgeDataManager.shared.saveFridgeItems(fridgeItems)
-        }
-        
-        @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
-            performSegue(withIdentifier: "showAddItem", sender: nil)
-        }
-        
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "showAddItem" {
-                if let navController = segue.destination as? UINavigationController,
-                   let addItemVC = navController.topViewController as? AddEditItemViewController {
-                    addItemVC.delegate = self
-                }
-            } else if segue.identifier == "showEditItem" {
-                if let navController = segue.destination as? UINavigationController,
-                   let editItemVC = navController.topViewController as? AddEditItemViewController,
-                   let indexPath = sender as? IndexPath {
-                    editItemVC.delegate = self
-                    editItemVC.itemToEdit = fridgeItems[indexPath.row]
-                    editItemVC.editingIndex = indexPath.row
-                }
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FridgeItemCell")
+    }
+    
+    private func loadFridgeItems() {
+        fridgeItems = FridgeDataManager.shared.loadFridgeItems()
+        tableView.reloadData()
+    }
+    
+    private func saveFridgeItems() {
+        FridgeDataManager.shared.saveFridgeItems(fridgeItems)
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "showAddItem", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAddItem" {
+            if let navController = segue.destination as? UINavigationController,
+               let addItemVC = navController.topViewController as? AddEditItemViewController {
+                addItemVC.delegate = self
+            }
+        } else if segue.identifier == "showEditItem" {
+            if let navController = segue.destination as? UINavigationController,
+               let editItemVC = navController.topViewController as? AddEditItemViewController,
+               let indexPath = sender as? IndexPath {
+                editItemVC.delegate = self
+                editItemVC.itemToEdit = fridgeItems[indexPath.row]
+                editItemVC.editingIndex = indexPath.row
             }
         }
     }
+}
 
-    // MARK: - TableView DataSource and Delegate
-
-
-    // MARK: - AddEditItemDelegate
-    extension FridgeViewController: AddEditItemDelegate {
-        func didAddItem(_ item: FridgeItem) {
-            fridgeItems.append(item)
-            saveFridgeItems()
-            tableView.reloadData()
+// MARK: - TableView DataSource and Delegate
+extension FridgeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fridgeItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FridgeItemCell", for: indexPath)
+        let item = fridgeItems[indexPath.row]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        
+        var subtitle = "Quantity: \(item.quantity)"
+        if let expirationDate = item.expirationDate {
+            subtitle += " • Expires: \(dateFormatter.string(from: expirationDate))"
         }
         
-        func didUpdateItem(_ item: FridgeItem, at index: Int) {
-            fridgeItems[index] = item
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = subtitle
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "showEditItem", sender: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            fridgeItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
             saveFridgeItems()
-            tableView.reloadData()
         }
     }
+}
+
+// MARK: - AddEditItemDelegate
+extension FridgeViewController: AddEditItemDelegate {
+    func didAddItem(_ item: FridgeItem) {
+        fridgeItems.append(item)
+        saveFridgeItems()
+        tableView.reloadData()
+    }
+    
+    func didUpdateItem(_ item: FridgeItem, at index: Int) {
+        fridgeItems[index] = item
+        saveFridgeItems()
+        tableView.reloadData()
+    }
+}
 
