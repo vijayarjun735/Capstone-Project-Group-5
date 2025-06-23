3486b4ad273d40855a6650ed8eaae599de8b76f5
Arjun V <vijayarjun735@gmail.com>
2025-06-22 22:11:32 -0400

    #11 Colour-Code Items
    #11 closed

--- ChillCheck/ChillCheck/ViewController.swift
+++ ChillCheck/ChillCheck/ViewController.swift
@@ -16,18 +16,23 @@ class FridgeViewController: UIViewController {
     var fridgeItems: [FridgeItem] = []
     var filteredFridgeItems: [FridgeItem] = []
     var isSearching = false
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         loadFridgeItems()
+      
         applyTheme()
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         loadFridgeItems()
+      
         applyTheme()
+        
     }
     
     private func setupUI() {
@@ -37,23 +42,107 @@ class FridgeViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FridgeItemCell")
-
+        
+    
         searchBar.delegate = self
         searchBar.placeholder = "Search fridge items..."
         searchBar.showsCancelButton = false
-
+     
+   
         setupMenuButton()
-
+       
     }
     
+    private func setupMenuButton() {
+        let menuButton = UIBarButtonItem(
+            image: UIImage(systemName: "line.horizontal.3"),
+            style: .plain,
+            target: self,
+            action: #selector(menuButtonTapped)
+        )
+        navigationItem.leftBarButtonItem = menuButton
+    }
+        @objc private func menuButtonTapped() {
+        performSegue(withIdentifier: "showMenu", sender: nil)
+    }
+    private func toggleDarkMode() {
+        let currentMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+        UserDefaults.standard.set(!currentMode, forKey: "isDarkMode")
+        applyTheme()
+    }
+    private func applyTheme() {
+        let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+        
+        if #available(iOS 13.0, *) {
+            view.window?.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
+        } else {
+            navigationController?.navigationBar.barStyle = isDarkMode ? .black : .default
+            tableView.backgroundColor = isDarkMode ? .black : .white
+        }
+    }
+    
+    private func confirmDeleteAllData() {
+        let alertController = UIAlertController(
+            title: "Delete All Data",
+            message: "Are you sure you want to delete all fridge items? This action cannot be undone.",
+            preferredStyle: .alert
+        )
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            self.deleteAllData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
+    }
+    
+    private func deleteAllData() {
+        fridgeItems.removeAll()
+        filteredFridgeItems.removeAll()
+        saveFridgeItems()
+        tableView.reloadData()
+        
+        let successAlert = UIAlertController(
+            title: "Success",
+            message: "All data has been deleted.",
+            preferredStyle: .alert
+        )
+        successAlert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(successAlert, animated: true)
+    }
     
     private func loadFridgeItems() {
         fridgeItems = FridgeDataManager.shared.loadFridgeItems()
+        filteredFridgeItems = fridgeItems
         tableView.reloadData()
     }
     
     private func saveFridgeItems() {
         FridgeDataManager.shared.saveFridgeItems(fridgeItems)
+    }
+    
+    private func getExpirationColor(for item: FridgeItem) -> UIColor {
+        guard let expirationDate = item.expirationDate else {
+            return .label
+        }
+        
+        let calendar = Calendar.current
+        let today = Date()
+        let daysUntilExpiration = calendar.dateComponents([.day], from: today, to: expirationDate).day ?? 0
+        
+        if daysUntilExpiration < 0 {
+            return .red
+        } else if daysUntilExpiration <= 3 {
+            return .red
+        } else if daysUntilExpiration <= 7 {
+            return .orange
+        } else {
+            return .systemGreen
+        }
     }
     
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
@@ -71,12 +160,10 @@ class FridgeViewController: UIViewController {
                let editItemVC = navController.topViewController as? AddEditItemViewController,
                let indexPath = sender as? IndexPath {
                 editItemVC.delegate = self
-                editItemVC.itemToEdit = fridgeItems[indexPath.row]
-                editItemVC.editingIndex = indexPath.row
-            }
-        }
+
     }
 }
+
 
 extension FridgeViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -110,31 +197,16 @@ extension FridgeViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
     }
 }
+
+
 // MARK: - TableView DataSource and Delegate
 extension FridgeViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fridgeItems.count
+        return isSearching ? filteredFridgeItems.count : fridgeItems.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FridgeItemCell", for: indexPath)
-        let item = fridgeItems[indexPath.row]
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
-        
-        var subtitle = "Quantity: \(item.quantity)"
-        if let expirationDate = item.expirationDate {
-            subtitle += " • Expires: \(dateFormatter.string(from: expirationDate))"
-        }
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = subtitle
-        cell.accessoryType = .disclosureIndicator
-        
-        return cell
-    }
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
@@ -143,7 +215,16 @@ extension FridgeViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            fridgeItems.remove(at: indexPath.row)
+            if isSearching {
+                let itemToDelete = filteredFridgeItems[indexPath.row]
+                if let originalIndex = fridgeItems.firstIndex(where: { $0.id == itemToDelete.id }) {
+                    fridgeItems.remove(at: originalIndex)
+                }
+                filteredFridgeItems.remove(at: indexPath.row)
+            } else {
+                fridgeItems.remove(at: indexPath.row)
+            }
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
             saveFridgeItems()
         }
@@ -155,13 +236,41 @@ extension FridgeViewController: AddEditItemDelegate {
     func didAddItem(_ item: FridgeItem) {
         fridgeItems.append(item)
         saveFridgeItems()
+        if isSearching {
+            let searchText = searchBar.text ?? ""
+            if item.name.lowercased().contains(searchText.lowercased()) ||
+               item.category.lowercased().contains(searchText.lowercased()) {
+                filteredFridgeItems.append(item)
+            }
+        }
         tableView.reloadData()
     }
     
     func didUpdateItem(_ item: FridgeItem, at index: Int) {
         fridgeItems[index] = item
         saveFridgeItems()
+        if isSearching {
+            if let filteredIndex = filteredFridgeItems.firstIndex(where: { $0.id == item.id }) {
+                let searchText = searchBar.text ?? ""
+                if item.name.lowercased().contains(searchText.lowercased()) ||
+                   item.category.lowercased().contains(searchText.lowercased()) {
+                    filteredFridgeItems[filteredIndex] = item
+                } else {
+                    filteredFridgeItems.remove(at: filteredIndex)
+                }
+            }
+        }
         tableView.reloadData()
+    }
+}
+
+extension FridgeViewController: MenuDelegate {
+    func didSelectDarkModeToggle() {
+        toggleDarkMode()
+    }
+    
+    func didSelectDeleteAllData() {
+        confirmDeleteAllData()
     }
 }
 
